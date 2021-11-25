85cb3fc9d099ea8db4044b3c5293c13b744739db
Tom Westcott <tom@cyberdummy.co.uk>
2021-11-25 19:50:50 +0000

    Organize util commands some

--- .config/dotdummy/aliases.sh
+++ .config/dotdummy/aliases.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 # Audio
 alias volu='pactl -- set-sink-volume @DEFAULT_SINK@ +10%'
 alias vold='pactl -- set-sink-volume @DEFAULT_SINK@ -10%'
@@ -12,14 +14,20 @@ alias ls='ls --color=auto'
 alias yank='TZ='America/New_York' date'
 alias afk='i3lock -t -i ~/.local/share/wallpapers/cowboy-4k.png'
 # Get a command from recent history and copy to clipboard
-alias hist="fc -l -n -r -3000 | awk '{\$1=\$1};1' | fzf --print0 | xsel -b -i"
-alias dark="sed -i 's/colors: \*light/colors: \*dark/g' ~/.config/alacritty/alacritty.yml"
-alias light="sed -i 's/colors: \*dark/colors: \*light/g' ~/.config/alacritty/alacritty.yml"
-alias sumcol="paste -sd+ - | bc"
+alias hist="dummy-util hist"
+alias dark="~/.dynamic-colors/bin/dynamic-colors switch solarized-dark"
+alias light="~/.dynamic-colors/bin/dynamic-colors switch solarized-light"
+alias sumcol="dummy-util sumcol"
 alias tunes="dummy audio chill"
 alias lpr="lpr -o fit-to-page"
 alias lpr2="lpr -o fit-to-page -#2"
 alias open="xdg-open"
+alias ff="dummy ff"
+alias slacker="dummy background flatpak run com.slack.Slack"
+alias zoomer="dummy background flatpak run us.zoom.Zoom"
+alias skyper="dummy background com.skype.Client"
+alias mcraft="dummy background com.mojang.Minecraft"
+alias vwiki="vim -S ~/.vim/sessions/vimwiki.vim"
 
 tremote(){
     systemctl --user start transmission
@@ -27,62 +35,10 @@ tremote(){
 }
 export -f tremote
 
-# open firefox
-ff(){
-  nohup firefox -p Tom --class browser-tom </dev/null &>/dev/null & disown
-}
-# slack
-slacker(){
-  nohup flatpak run com.slack.Slack </dev/null &>/dev/null & disown
-}
-
-# skype
-skyper() {
-  nohup flatpak run com.skype.Client </dev/null &>/dev/null & disown
-}
-#zoom
-zoomer() {
-  nohup flatpak run us.zoom.Zoom </dev/null &>/dev/null & disown
-}
 # older servers dont know the tmux TERM type
 ssh(){
     local LOCAL_TERM=$(echo -n "$TERM" | sed -e s/tmux/screen/)
     env TERM=$LOCAL_TERM ssh "$@"
-}
-mcraft(){
-    nohup flatpak run com.mojang.Minecraft </dev/null &>/dev/null & disown
-}
-# vim wiki
-vwiki() {
-  vim -S ~/.vim/sessions/vimwiki.vim "$@"
-}
-
-# Transform a zoom meeting URL into one to launch zoom with
-zoomzoom(){
-    local id=$(echo "$1" | cut -d '/' -f5 | cut -d '?' -f1)
-    local query=$(echo "$1" | cut -d '?' -f2)
-    local url="zoommtg://zoom.us/join?action=join&confno=$id&$query"
-
-    flatpak run us.zoom.Zoom "$url"
-}
-
-countdown(){
-    date1=$((`date +%s` + $1));
-    while [ "$date1" -ge `date +%s` ]; do
-    ## Is this more than 24h away?
-    days=$(($(($(( $date1 - $(date +%s))) * 1 ))/86400))
-    echo -ne "$days day(s) and $(date -u --date @$(($date1 - `date +%s`)) +%H:%M:%S)\r";
-    sleep 0.1
-    done
-}
-
-stopwatch(){
-    date1=`date +%s`;
-    while true; do
-    days=$(( $(($(date +%s) - date1)) / 86400 ))
-    echo -ne "$days day(s) and $(date -u --date @$((`date +%s` - $date1)) +%H:%M:%S)\r";
-    sleep 0.1
-    done
 }
 
 local_file="${HOME}/.config/dotdummy/aliases_local.sh"
