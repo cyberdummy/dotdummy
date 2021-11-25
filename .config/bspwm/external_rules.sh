7aa5d55cd2c2135a1df21f2042d2cb16ec8f0555
Tom Westcott <tom@cyberdummy.co.uk>
2021-11-25 10:53:21 +0000

    Assign bspwm desktops

---
+++ .config/bspwm/external_rules.sh
@@ -0,0 +1,24 @@
+#! /bin/sh
+
+wid=$1
+class=$2
+instance=$3
+consequences=$4
+
+shopt -s nocasematch
+
+if [[ "$class" == *"Minecraft"* ]] || [[ "$class" == *"minecraft"* ]]; then
+    echo "desktop=five"
+    exit 0
+fi
+
+# slow to set props
+if [[ "$class" == "" ]]; then
+  sleep 0.5
+  wm_class=($(xprop -id $wid | grep "WM_CLASS" | grep -Po '"\K[^,"]+'))
+
+  if [[ "$wm_class" == *"Minecraft"* ]] || [[ "$wm_class" == *"minecraft"* ]]; then
+      echo "desktop=five"
+      exit 0
+  fi
+fi
