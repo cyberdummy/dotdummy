83a246427a40621c5c275e7ce670e05875ea5364
Tom Westcott <tom@cyberdummy.co.uk>
2026-07-17 17:52:44 +0100

    Update ruby path

--- .config/dotdummy/exports.sh
+++ .config/dotdummy/exports.sh
@@ -3,7 +3,7 @@
 export EDITOR=nvim
 export BROWSER=firefox
 export SSH_AUTH_SOCK=/run/user/$(id -u)/gnupg/S.gpg-agent.ssh
-export PATH=~/.local/bin:~/.config/composer/vendor/bin:~/.local/share/gem/ruby/3.0.0/bin:~/code/go/gopath/bin:~/.cargo/bin:$PATH
+export PATH=~/.local/bin:~/.config/composer/vendor/bin:~/.local/share/gem/ruby/3.4.0/bin:~/code/go/gopath/bin:~/.cargo/bin:$PATH
 export GOPATH="${HOME}/code/go/gopath"
 export PERL_DESTRUCT_LEVEL=2 # stops urxvt exit crash on arch for some reason
 export UID
